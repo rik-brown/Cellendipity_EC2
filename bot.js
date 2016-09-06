229e4775eab258482b4ca4731e6947d8348880f0
rik-brown <richard.brown.p5js@gmail.com>
2016-09-06 23:58:50 +0200

    Tidying up. This build is deployed to EC2

--- bot.js
+++ bot.js
@@ -7,27 +7,28 @@ var exec = require('child_process').exec;
 var fs = require('fs');
 
 postTweet();
-// 1000 milliseconds * 60 seconds * 120 minutes = 2 hours
-setInterval(postTweet, 1000*60*120);
+// Interval: 3 hours
+// 1000 milliseconds * 60 seconds * 60 minutes * 3 hours
+setInterval(postTweet, 1000*60*60*3);
 
 function postTweet() {
-  var cmd = 'cellendipity\cellendipity';
+  var cmd = 'cellendipity_EC2/Cellendipity_EC2';
   exec (cmd, processing);
 
   function processing() {
     console.log('Uploading media...');
-    var filename = 'cellendipity/output.png';
+    var filename = 'cellendipity_EC2/output.png';
   	var params = {
       encoding: 'base64'
   	}
     var b64 = fs.readFileSync(filename, params);
-    
+
     T.post('media/upload', { media_data: b64 }, uploaded);
 
     function uploaded(err, data, response) {
       var id = data.media_id_string;
       console.log("id=" + id)
-  	  var tweet = {status: 'A generated life story. #cellendipity', media_ids: [id]}
+  	  var tweet = {status: 'Generated cellular life stories. #cellendipity', media_ids: [id]}
       T.post('statuses/update', tweet, tweeted);
     }
 
